433c5d5e80023948b18b48310b0ad4558efc1ab9
ryosuke nakaishi(Windows) <acna2676@gmail.com>
2022-11-26 14:44:30 +0900

    feat: chapter4

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,36 @@
 
+# import unittest
+
+# from selenium import webdriver
+
+
+# class NewVisitorTest(unittest.TestCase):
+#     def setUp(self):
+#         # self.browser = webdriver.Firefox()
+#         self.browser = webdriver.Chrome()
+
+#     def tearDown(self):
+#         self.browser.quit()
+
+#     def test_can_start_a_list_and_retrieve_it_later(self):
+#         # Edith has heard about a cool new online to-do app. She goes
+#         # to check out its homepage
+#         self.browser.get('http://localhost:8000')
+#         # She notices the page title and header mention to-do lists
+#         self.assertIn('To-Do', self.browser.title)
+#         self.fail('Finish the test!')
+
+#         # She is invited to enter a to-do item straight away
+#         # [...rest of comments as before]
+# if __name__ == '__main__':
+#     unittest.main(warnings='ignore')
+
+import time
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -18,9 +47,36 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
+        # header_text = self.browser.find_element_by_tag_name('h1').text
+        header_text = self.browser.find_element(By.TAG_NAME, "h1").text
+        self.assertIn('To-Do', header_text)
+        # She is invited to enter a to-do item straight away
+        # inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list table
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
+        )
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very
+        # methodical)
         self.fail('Finish the test!')
 
-        # She is invited to enter a to-do item straight away
-        # [...rest of comments as before]
+        # The page updates again, and now shows both items on her list
+        # [...]
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
