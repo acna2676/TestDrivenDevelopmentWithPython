78f346085e72f1a8492fec911b19f30a36798533
Nakaishi Ryosuke <acna2676@gmail.com>
2022-11-27 16:50:32 +0900

    make functional_tests an app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,39 +1,12 @@
-
-# import unittest
-
-# from selenium import webdriver
-
-
-# class NewVisitorTest(unittest.TestCase):
-#     def setUp(self):
-#         # self.browser = webdriver.Firefox()
-#         self.browser = webdriver.Chrome()
-
-#     def tearDown(self):
-#         self.browser.quit()
-
-#     def test_can_start_a_list_and_retrieve_it_later(self):
-#         # Edith has heard about a cool new online to-do app. She goes
-#         # to check out its homepage
-#         self.browser.get('http://localhost:8000')
-#         # She notices the page title and header mention to-do lists
-#         self.assertIn('To-Do', self.browser.title)
-#         self.fail('Finish the test!')
-
-#         # She is invited to enter a to-do item straight away
-#         # [...rest of comments as before]
-# if __name__ == '__main__':
-#     unittest.main(warnings='ignore')
-
 import time
-import unittest
 
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         # self.browser = webdriver.Firefox()
         self.browser = webdriver.Chrome()
@@ -47,8 +20,12 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get('http://localhost:8000')
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get(self.live_server_url)
+        # self.browser.get('http://localhost:8000')
         inputbox = self.browser.find_element(By.ID, "id_new_item")
+        inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
         self.check_for_row_in_list_table('1: Buy peacock feathers')
@@ -62,5 +39,3 @@ class NewVisitorTest(unittest.TestCase):
 
         # The page updates again, and now shows both items on her list
         # [...]
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
