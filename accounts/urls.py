4b915dc0db87f147652cf40925df3e464c23a51b
ryosuke nakaishi(Windows) <acna2676@gmail.com>
2022-12-02 18:23:08 +0900

    feat: chapter19

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import re_path
 
 from accounts import views
@@ -5,4 +6,5 @@ from accounts import views
 urlpatterns = [
     re_path(r'^send_login_email$', views.send_login_email, name='send_login_email'),
     re_path(r'^login$', views.login, name='login'),
+    re_path(r'^logout$', LogoutView.as_view(next_page='/'), name='logout'),  # upgrade for Django4.1
 ]
