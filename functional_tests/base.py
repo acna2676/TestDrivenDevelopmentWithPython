4307cb8b1e56870990351983423b74a8063b7d96
ryosuke nakaishi(Windows) <acna2676@gmail.com>
2022-12-03 22:31:13 +0900

    New FT for sharing, move session creation stuff to base

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,6 +1,10 @@
 import os
 import time
 
+from django.conf import settings
+from django.contrib.auth import (BACKEND_SESSION_KEY, SESSION_KEY,
+                                 get_user_model)
+from django.contrib.sessions.backends.db import SessionStore
 # from django.test import LiveServerTestCase
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
@@ -9,6 +13,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 MAX_WAIT = 10
+User = get_user_model()
 
 
 class FunctionalTest(StaticLiveServerTestCase):
@@ -91,3 +96,18 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.get_item_input_box().send_keys(Keys.ENTER)
         item_number = num_rows + 1
         self.wait_for_row_in_list_table(f'{item_number}: {item_text}')
+
+    def create_pre_authenticated_session(self, email):
+        user = User.objects.create(email=email)
+        session = SessionStore()
+        session[SESSION_KEY] = user.pk
+        session[BACKEND_SESSION_KEY] = settings.AUTHENTICATION_BACKENDS[0]
+        session.save()
+        # to set a cookie we need to first visit the domain.
+        # 404 pages load the quickest!
+        self.browser.get(self.live_server_url + "/404_no_such_url/")
+        self.browser.add_cookie(dict(
+            name=settings.SESSION_COOKIE_NAME,
+            value=session.session_key,
+            path='/',
+        ))
